929e721322f7b654d87e65acae279e7ac609306e
David E. Barrera <dbarrerap@live.com>
2021-10-04 09:23:01 -0500

    Code Scaffolding

---
+++ Data/ReservationsContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReservationsAPI.Models;
+
+    public class ReservationsContext : DbContext
+    {
+        public ReservationsContext (DbContextOptions<ReservationsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+        public DbSet<Establishment> Establishment { get; set; }
+
+        public DbSet<Evento> Evento { get; set; }
+
+        public DbSet<Reservation> Reservation { get; set; }
+    }
--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ReservationsAPI
 {
@@ -32,6 +33,13 @@ namespace ReservationsAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReservationsAPI", Version = "v1" });
             });
+
+            services.AddDbContext<ReservationsContext>(options =>
+                    options.UseMySql(
+                        Configuration.GetConnectionString("MariaDbContext"),
+                        ServerVersion.AutoDetect(Configuration.GetConnectionString("MariaDbContext"))
+                    )
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
