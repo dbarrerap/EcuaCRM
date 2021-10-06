72655a1a7e9c1dfdf9a861f4e2bc8dbf6557bb41
David E. Barrera <dbarrerap@live.com>
2021-10-06 13:52:10 -0500

    Add Models to Context

--- Data/ReservationsContext.cs
+++ Data/ReservationsContext.cs
@@ -13,14 +13,12 @@ using ReservationsAPI.Models;
         }
 
         public DbSet<Cliente> Cliente { get; set; }
-
         public DbSet<Establishment> Establishment { get; set; }
-
         public DbSet<Evento> Evento { get; set; }
-
         public DbSet<Reservation> Reservation { get; set; }
-
         public DbSet<Provider> Provider { get; set; }
-
         public DbSet<Product> Product { get; set; }
-    }
+        public DbSet<Contacto> Contacto { get; set; }
+        public DbSet<EstablishmentContact> EstablishmentContact { get; set; }
+        public DbSet<ProviderContact> ProviderContact { get; set; }
+}
